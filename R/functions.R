7faf1efe629b495f4c694a944f3cad397e381edb
Jakez <jakez.rolland@redplanr.com>
2019-03-06 00:20:20 +0100

    fix call in errormessage

--- R/functions.R
+++ R/functions.R
@@ -3,12 +3,10 @@
 #' @param err error message in the try catch
 #' @param functionName name of function the try catch is located
 #' @param step name of the step in the function
-#' @return
 #' @export
-#'
-#' @examples
+
 onError<-function(err,functionName,   step){
   message_traceback<-err$message;
   errormessage = paste(paste("at ",functionName," :: ",step,sep=""),message_traceback,sep='\n')
-  stop(errormessage)
+  stop(errormessage,call. = FALSE)
 }
